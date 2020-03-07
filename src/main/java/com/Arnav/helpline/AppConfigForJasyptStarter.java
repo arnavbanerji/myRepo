c685dab87b950543180941e11be73530fa6e19bf
arnavbanerji <arnav.banerji@yahoo.com>
2020-03-07 21:13:06 +0530

    encryt client secret

---
+++ src/main/java/com/Arnav/helpline/AppConfigForJasyptStarter.java
@@ -0,0 +1,11 @@
+/*
+package com.Arnav.helpline;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("application.yml")
+public class AppConfigForJasyptStarter {
+}
+*/
---
+++ src/main/java/com/Arnav/helpline/service/PropertyServiceForJasyptStarter.java
@@ -0,0 +1,23 @@
+/*
+package com.Arnav.helpline.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+//service bean to retrieve the valus of
+@Service
+public class PropertyServiceForJasyptStarter {
+
+    @Value("${encrypted.property}")
+    private String property;
+
+    public String getProperty() {
+        return property;
+    }
+
+    public String getPasswordUsingEnvironment(Environment environment) {
+        return environment.getProperty("encrypted.property");
+    }
+}
+*/
